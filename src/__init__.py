2af45676cd5bc74f760063d14553cbe1c14ec895
satvik <nagarsatvik@gmail.com>
2024-10-07 16:28:08 +0530

    logging and exception

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+#             use of __init__.py
+# it consider src(folder) as package itself so,  we can import it anywhere
+##    This file is executed when the package is imported
+# file can also contain documentation for your package (version information, and author credits)
